50036e2c0117d5d02fe54bd7a03c2b448fa65fe7
Nivaashini-T <24f2007947@ds.study.iitm.ac.in>
2025-06-14 16:04:01 +0530

    Remove extra files from Git tracking

--- check_emb.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("knowledge_base.db")
-cursor = conn.cursor()
-cursor.execute("SELECT COUNT(*) FROM discourse_chunks WHERE embedding IS NOT NULL")
-print("Embedded chunks:", cursor.fetchone()[0])
-conn.close()
